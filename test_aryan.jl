5fbebe697d065cd9b049fcfda928c9c284ff9f2b
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-08-09 02:20:03 -0700

    `ybuses` now consistent for IEEE `14, 30, 57, 118` Bus systems across sparse and dense implementations.
    Now what's the deal with the mismatching Jacobian?

--- test_aryan.jl
+++ test_aryan.jl
@@ -5,41 +5,41 @@ using CSV
 using DataFrames
 using LinearAlgebra
 
-include("src/Helper_Functions.jl")
-include("src/Ybus_Builder.jl")
-include("src/IEEE_CDF_Parser.jl")
-include("src/SparseTechniques_Functions.jl")
-include("src/Jacobian_Builder.jl")
-folderInput = "data/"
+include("src/Helper_Functions.jl");
+include("src/Ybus_Builder.jl");
+include("src/IEEE_CDF_Parser.jl");
+include("src/SparseTechniques_Functions.jl");
+include("src/Jacobian_Builder.jl");
+folderInput = "data/";
 folder_processedData = "processedData/";
 # systemName = "IEEE_14";
-# systemName = "IEEE_118"
-systemName = "IEEE_30";
+systemName = "IEEE_118"
+# systemName = "IEEE_30";
+# systemName = "IEEE_57"
 
 createFolderIfNotExisting(systemName, folder_processedData)
 
 fileType_CDFFile = ".txt";
 filename_CDFFile = folderInput*systemName*"/"*systemName*"_Data"*fileType_CDFFile
-CDF_DF_List = CDF_Parser(filename_CDFFile, saveTables = true, saveLocation = folder_processedData);
-busData = CDF_DF_List[2];
-# vscodedisplay(busData)
-# CDF_DF_List_pu = CDF_pu_Converter(CDF_DF_List, saveTables = true, saveLocation = folder_processedData);
+CDF_DF_List = CDF_Parser(filename_CDFFile);
 CDF_DF_List_pu = CDF_pu_Converter(CDF_DF_List);
 
 systemName = extractSystemName(CDF_DF_List)
 
 busData_pu = CDF_DF_List_pu[2];
-# vscodedisplay(busData_pu)
 branchData_pu = CDF_DF_List_pu[3];
 
 sparYBus = constructSparseYBus(CDF_DF_List_pu);
-NYBus = sparYBus.NVec; MYBus = sparYBus.MVec; nnzYBus = sparYBus.nnzVec;
+ybusSpar2Full = spar2Full(sparYBus, readMethod="col-wise")
 
-ybusFull = spar2Full(sparYBus, readMethod="col-wise")
-# vscodedisplay(ybusFull)
-ybus, BMatrix, b, A, branchNames, E   = ybusGenerator(CDF_DF_List_pu, saveTables=false);
-# vscodedisplay(ybus)
-# ybus_ByTypes, BMatrix_ByTypes, b_ByTypes, A_ByTypes, branchNames_ByTypes, E_ByTypes = ybusGenerator(CDF_DF_List_pu, sortBy="busTypes", saveTables=true, saveLocation=folder_processedData);
+ybusResults = ybusGenerator(CDF_DF_List_pu, saveTables=true);
+ybusDense = ybusResults.ybus
+E = ybusResults.E ;
+
+diff = ybusDense-ybusSpar2Full
+non_zero_elements = [(diff[i, j], i, j) for i in 1:size(diff, 1), j in 1:size(diff, 2) if diff[i, j] != 0]
+
+@test ybusSpar2Full ≈ ybusDense
 
 PSpecified, QSpecified, V, delta, listOfSlackBuses, listOfPVBuses, 
 listOfPQBuses, listOfNonSlackBuses, nSlack, nPV, nPQ = initializeVectors_pu(CDF_DF_List_pu);
@@ -70,7 +70,7 @@ JRegular = constructJacobian(CDF_DF_List_pu, P, Q, V, delta, ybus, E=E);
 # 0 2 3 0 4 0 0 0 0 35];
 
 qluJ = sparLU(sparJ);
-QJ, LJ, UK, αJ, fill_insJ = qluJ.Q, qluJ.L, qluJ.U, qluJ.α, qluJ.fills;
+QJ, LJ, UJ, αJ, fill_insJ = qluJ.Q, qluJ.L, qluJ.U, qluJ.α, qluJ.fills;
 
 αJ
 fill_insJ
