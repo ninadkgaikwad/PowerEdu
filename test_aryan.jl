09a099c30a278212ea3a4c9c906d4dd24109614a
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-08-14 01:36:02 -0700

    Aight calling it a day today. Results same as what was achieved 10 months back.
    Need to face my demons and finally implement that bus switching thing next time.

--- test_aryan.jl
+++ test_aryan.jl
@@ -14,13 +14,14 @@ include("src/IEEE_CDF_Parser.jl");
 include("src/SparseTechniques_Functions.jl");
 include("src/Jacobian_Builder.jl");
 include("src/LU_Factorization.jl");
+include("src/BasicPowerFlow_Functions.jl")
 
 folderInput = "data/";
 folder_processedData = "processedData/";
 
-# systemName = "IEEE_14";
+systemName = "IEEE_14";
 # systemName = "IEEE_118"
-systemName = "IEEE_30";
+# systemName = "IEEE_30";
 # systemName = "IEEE_57"
 
 
@@ -33,6 +34,16 @@ filename_CDFFile = folderInput*systemName*"/"*systemName*"_Data"*fileType_CDFFil
 CDF_DF_List = CDF_Parser(filename_CDFFile);
 CDF_DF_List_pu = CDF_pu_Converter(CDF_DF_List);
 
-results = solveForPowerFlow_Sparse(CDF_DF_List_pu, verbose=true)
+results = solveForPowerFlow_Sparse(CDF_DF_List_pu, verbose=false)
+# busData = CDF_DF_List_pu[2];
+# mean(abs.(results.V - busData.Final_V_pu_Original))
+# mean(abs.(results.δ- busData.Final_A_deg_Original*π/180))
+# mean(abs.(results.P - busData.Gen_MW))
+# mean(abs.(results.Q - busData.Gen_MVAR))
 
 
+# x = range(0, 10, length=100)
+# y1 = sin.(x)
+# y2 = cos.(x)
+# plot(x, [y1 y2], title="Trigonometric functions", label=["sin(x)" "cos(x)"], linewidth=3)
+plotBuswiseDifferences(CDF_DF_List_pu, results, savePlots=true)
