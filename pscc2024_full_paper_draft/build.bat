67e1b2298777594d732853f62a2b1b333597ac91
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-08-15 00:29:07 -0700

    an attempt to use the `standalone` package was made

---
+++ pscc2024_full_paper_draft/build.bat
@@ -0,0 +1,6 @@
+@echo off
+set TEXINPUTS=.\outputs\\;
+echo Konichiwa Minasan!
+@REM pdflatex main.tex
+@REM echo Script has finished running.
+
---
+++ pscc2024_full_paper_draft/build.sh
@@ -0,0 +1,6 @@
+@echo off
+set TEXINPUTS=./outputs//;
+echo Konichiwa Minasan!
+pdflatex main.tex
+echo Script has finished running.
+
