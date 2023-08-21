f6492ced3a3ef1c30ef112e06eb609db4bf5bb7d
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-08-20 22:29:04 -0700

    m

--- test_aryan.jl
+++ test_aryan.jl
@@ -7,6 +7,7 @@ using Cthulhu
 using BenchmarkTools
 using Plots
 using Statistics
+using LaTeXStrings
 
 include("src/Helper_Functions.jl");
 include("src/Ybus_Builder.jl");
@@ -24,9 +25,6 @@ systemName = "IEEE_14";
 # systemName = "IEEE_30";
 # systemName = "IEEE_57"
 
-
-
-
 createFolderIfNotExisting(systemName, folder_processedData)
 
 fileType_CDFFile = ".txt";
@@ -35,15 +33,5 @@ CDF_DF_List = CDF_Parser(filename_CDFFile);
 CDF_DF_List_pu = CDF_pu_Converter(CDF_DF_List);
 
 results = solveForPowerFlow_Sparse(CDF_DF_List_pu, verbose=false)
-# busData = CDF_DF_List_pu[2];
-# mean(abs.(results.V - busData.Final_V_pu_Original))
-# mean(abs.(results.δ- busData.Final_A_deg_Original*π/180))
-# mean(abs.(results.P - busData.Gen_MW))
-# mean(abs.(results.Q - busData.Gen_MVAR))
 
-
-# x = range(0, 10, length=100)
-# y1 = sin.(x)
-# y2 = cos.(x)
-# plot(x, [y1 y2], title="Trigonometric functions", label=["sin(x)" "cos(x)"], linewidth=3)
-plotBuswiseDifferences(CDF_DF_List_pu, results, savePlots=true)
+plotBuswiseDifferences(CDF_DF_List_pu, results, savePlots=false)
